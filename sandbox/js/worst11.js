eaf6cd7439dac7b78920639549c48bc36bf49e59
John Aspinall <john.aspinall73@gmail.com>
2020-06-14 22:20:05 +0100

    DAY 25: Adding JSON  squad list to the screen with appendChild() function

--- sandbox/js/worst11.js
+++ sandbox/js/worst11.js
@@ -1,20 +1,34 @@
-const players
+const squad = document.querySelector('.squad');
 
 const data = new XMLHttpRequest();
 data.open('GET', 'json/worst11.json');
 data.send();
 
+const addPlayer = (name) => {
+    let div = document.createElement('div')
+    div.textContent = name;
+    return div;
+}
+
 const getPlayers = () => {
     data.addEventListener('readystatechange', () => {
         if ( data.readyState === 4 & data.status === 200) {
-            const playersObj = JSON.parse(data.responseText);
-            console.log(playersObj);
-            console.log(typeof playersObj);
+            const playersObj = JSON.parse(data.responseText); // Parse the JSON into players object
+            const players = Object.entries(playersObj); // Convert object into an array so can loop round data with forEach method
+            let playerItem = document.createElement('div');
+            players.forEach((player, index) => {
+                const playerName = player[1].name;
+                console.log(playerName);
+                squad.appendChild(addPlayer(playerName));
+                
+
+            });
         }
         else if (data.readyState === 4) {
             console.log('There was an error fetching the data');
         }
-    })
+    })    
+
 }
 
 getPlayers();
