bbbe1b94348eb4d0bc5dd8a0061665f56dcf6e68
John Aspinall <john.aspinall73@gmail.com>
2020-06-16 00:01:07 +0100

    Day 25:  Modified JSON file to record player number

--- sandbox/js/worst11.js
+++ sandbox/js/worst11.js
@@ -4,9 +4,9 @@ const data = new XMLHttpRequest();
 data.open('GET', 'json/worst11.json');
 data.send();
 
-const addPlayer = (name) => {
+const addPlayer = (name, number) => {  
     let div = document.createElement('div')
-    div.textContent = name;
+    div.textContent = number;
     return div;
 }
 
@@ -15,20 +15,17 @@ const getPlayers = () => {
         if ( data.readyState === 4 & data.status === 200) {
             const playersObj = JSON.parse(data.responseText); // Parse the JSON into players object
             const players = Object.entries(playersObj); // Convert object into an array so can loop round data with forEach method
-            let playerItem = document.createElement('div');
             players.forEach((player, index) => {
-                const playerName = player[1].name;
-                console.log(playerName);
-                squad.appendChild(addPlayer(playerName));
-                
-
+                let playerName = player[1].name;
+                let playerNumber = player[1].number;
+                console.log(playerNumber);
+                squad.appendChild(addPlayer(playerName, playerNumber));
             });
         }
         else if (data.readyState === 4) {
             console.log('There was an error fetching the data');
         }
     })    
-
 }
 
 getPlayers();
